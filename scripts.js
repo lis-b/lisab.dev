c315f94d92b4b48855307543fa78026b221eedb5
lis-b <45443128+lis-b@users.noreply.github.com>
2021-02-26 01:02:35 -0800

    fixed CSS transitions on page load & implemented localStorage for theme

--- scripts.js
+++ scripts.js
@@ -1,14 +1,3 @@
-const dark = 'dark-mode';
-const element = document.body;
-
-function setMode() {
-  if (localStorage[dark] === false) element.classList.remove('dark-mode');
-  element.classList.add('transitions');
-}
-
-function toggleDarkMode() {
-  element.classList.toggle('dark-mode');
-  localStorage[dark] = !localStorage[dark];
-}
-
-setMode();
+const bodyLoaded = () => {
+  document.body.classList.remove('stop-transitions');
+};
